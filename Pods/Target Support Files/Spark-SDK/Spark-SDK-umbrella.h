d92ac640e0e3eeaf16fe5f42df41b02a8eeee4e3
Jakk Sut <jakks@Jakks-iMac.local>
2017-09-05 00:38:38 +0700

    ‘init’

---
+++ Pods/Target Support Files/Spark-SDK/Spark-SDK-umbrella.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+
+#import "Spark-SDK-Bridging-Header.h"
+#import "Spark-SDK.h"
+#import "EventSource.h"
+#import "KeychainItemWrapper.h"
+#import "SparkCloud.h"
+#import "SparkDevice.h"
+#import "SparkEvent.h"
+#import "SparkSession.h"
+
+FOUNDATION_EXPORT double Spark_SDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char Spark_SDKVersionString[];
+
---
+++ VibrationController/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  VibrationController
+//
+//  Created by JakkritS on 6/8/2559 BE.
+//  Copyright © 2559 AppIllustrator. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
